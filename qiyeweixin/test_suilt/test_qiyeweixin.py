04d59cedbf9b55b8f3de899505f596f16dd8c67a
mengnachendu <dzxyjsjxym@163.com>
2022-05-31 15:35:30 +0800

    第一次提交

--- qiyeweixin/test_suilt/test_qiyeweixin.py
+++ qiyeweixin/test_suilt/test_qiyeweixin.py
@@ -1,15 +1,18 @@
+import yaml
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
-import yaml
-import pytest
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from faker import Faker
-from gaojie.utils_tool.logging_info import logger
-from gaojie.utils_tool.web_utils import click_exception_user_defined
+from qiyeweixin.utils_tool.logging_info import logger
+from qiyeweixin.utils_tool.web_utils import click_exception_user_defined
+import datetime
+import allure
+import pytest
+from selenium.webdriver.support.wait import WebDriverWait
 
-class TestCookieDemo:
+
+class TestQiYeWeiXin:
 
     #前置动作
     def setup_class(self):
@@ -17,8 +20,8 @@ class TestCookieDemo:
         self.driver.maximize_window()
         self.driver.implicitly_wait(3)
 
-        self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx?")
-        cookies = yaml.safe_load(open("./data/qiyeweixin_cookie.yaml"))
+        self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx")
+        cookies = yaml.safe_load(open("../data/qiyeweixin_cookie.yaml"))
         for i in cookies:
             self.driver.add_cookie(i)
 
@@ -26,16 +29,43 @@ class TestCookieDemo:
     def teardown_class(self):
         self.driver.quit()
 
+    def exception_handle(func):
+        def inner(*args, **kwargs):
+            try:
+                driver = args[0].driver
+                func(*args, **kwargs)
+            except:
+                strstamp = datetime.datetime.now()
+                fmt_time = strstamp.strftime("%Y%m%d_%H%M%S")
+
+                #定义异常截图存放路径，并进行截图操作
+                image_path = f"../image/image_{fmt_time}.png"
+                driver.save_screenshot(image_path)
+
+                #定义异常源码存放路径，并把异常源码保存到文件中
+                page_source_path = f"../page_source/page_source_{fmt_time}.html"
+                with open(page_source_path, 'w', encoding='utf-8') as f:
+                    f.write(driver.page_source)
+
+                #把异常截图、源码写入到测试报告中
+                logger.info("这里出现异常了")
+                allure.attach.file(image_path, "异常截图", attachment_type=allure.attachment_type.PNG)
+                allure.attach.file(page_source_path, "异常源码", attachment_type=allure.attachment_type.TEXT)
+
+                raise Exception
+        return inner
+
     #获取cookie并保存到文件中
     @pytest.mark.skip
     def test_get_cookie(self):
-        self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx?")
+        self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx")
         sleep(15)
         cookie = self.driver.get_cookies()
-        with open("./data/qiyeweixin_cookie.yaml", 'w', encoding='utf-8') as f:
+        with open("../data/qiyeweixin_cookie.yaml", 'w', encoding='utf-8') as f:
             yaml.safe_dump(cookie, f)
 
     #case1：在首页菜单:点击添加成员按钮后，添加成员
+    @exception_handle
     def test_add_member_1(self):
 
         #设置随机数
@@ -43,10 +73,11 @@ class TestCookieDemo:
         self.username = fake.name()
         self.acctid = fake.ssn()
         self.mobile = fake.phone_number()
+        self.mail = fake.postcode()
 
         #1、登录页面：登录
         logger.info("1、登录页面：登录")
-        self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx?")
+        self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx")
 
         #2、首页：点击添加成员按钮
         logger.info("2、首页：点击添加成员菜单")
@@ -57,7 +88,7 @@ class TestCookieDemo:
         self.driver.find_element(By.NAME, "username").send_keys(self.username)
         self.driver.find_element(By.ID, "memberAdd_acctid").send_keys(self.acctid)
         self.driver.find_element(By.ID, "memberAdd_biz_mail").clear()
-        self.driver.find_element(By.ID, "memberAdd_biz_mail").send_keys(self.mobile)
+        self.driver.find_element(By.ID, "memberAdd_biz_mail").send_keys(self.mail)
         self.driver.find_element(By.ID, "memberAdd_phone").send_keys(self.mobile)
         self.driver.find_element(By.XPATH, "//*[text()='保存']").click()
 
@@ -69,7 +100,7 @@ class TestCookieDemo:
 
         #5、清楚脏数据，成员信息页面：选中指定的成员后删除
         logger.info("5、成员信息页面：选中指定的成员后删除")
-        self.driver.find_element(By.XPATH, f"//*[text()='{self.username}']/../..//*[@class='ww_checkbox']").click()
+        self.driver.find_element(By.XPATH, f"//*[text()='{self.username}']/../..//*[@class='ww_checkbox1']").click()
         self.driver.find_element(By.XPATH, "//*[@class='ww_operationBar']//*[text()='删除']").click()
         self.driver.find_element(By.XPATH, "//*[text()='确认']").click()
 
@@ -90,7 +121,7 @@ class TestCookieDemo:
 
         # 1、登录页面：登录
         logger.info("1、登录页面：登录")
-        self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx?")
+        self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx")
 
         #2、首页：点击通讯录菜单
         logger.info("2、首页：点击通讯录菜单")
@@ -99,7 +130,7 @@ class TestCookieDemo:
         #3、添加成员列表页面：点击添加成员按键
         logger.info("3、添加成员列表页面：点击添加成员")
         WebDriverWait(self.driver, 10).until(
-            click_exception_user_defined(By.XPATH, "//*[@class='js_has_member']//*[text()='添加成员']"))
+            click_exception_user_defined(By.XPATH, "//*[text()='添加成员']"))
 
         # 4、添加成员信息页面：输入成员个人信息后保存
         logger.info("4、添加成员信息页面：输入成员个人信息后保存")
@@ -142,7 +173,7 @@ class TestCookieDemo:
 
         # 1、登录页面：登录
         logger.info("1、登录页面：登录")
-        self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx?")
+        self.driver.get("https://work.weixin.qq.com/wework_admin/loginpage_wx")
 
         # 2、首页：点击通讯录菜单
         logger.info("2、首页：点击通讯录菜单")
